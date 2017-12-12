48f9096d4fb2a87fc3c87893d41c43867ebfe0b3
elicia <elicia.erker@gmail.com>
2017-12-12 14:56:22 -0600

    adding some basic files

---
+++ lights.py
@@ -0,0 +1,53 @@
+# stuff for the light strip
+from bibliopixel.animation import BaseStripAnim
+from bibliopixel.drivers.driver_base import ChannelOrder
+import bibliopixel.colors as colors
+
+
+class StripTest(BaseStripAnim):
+    def __init__(self, led, start=0, end=-1):
+        # The base class MUST be initialized by calling super like this
+        super(StripTest, self).__init__(led, start, end)
+        # Create a color array to use in the animation
+        self._colors = [colors.Red, colors.Orange, colors.Yellow, colors.Green, colors.Blue, colors.Indigo]
+
+    def step(self, amt=1):
+        # Fill the strip, with each sucessive color
+        # for every led in the strip
+        for i in range(self._led.numLEDs):
+            self._led.set(i, self._colors[(self._step + i) % len(self._colors)])
+        # Increment the internal step by the given amount
+        self._step += amt
+
+
+"""
+A variation of the strip test.
+This animation will be played at the end of a level. The light strip will fill up with rainbow colours one
+light at a time, then flash rainbows for a bit
+"""
+class WinAnimation(BaseStripAnim):
+	def __init__(self, led, start=0, end=-1):
+		super(WinAnimation, self).__init__(led, start, end)
+		# set attributes step might need
+		self._colors = [colors.Red, colors.Orange, colors.Yellow, colors.Green, colors.Blue, colors.Indigo]
+
+	def step(self, amt=1):
+		# the light strip hasn't been filled with colour yet
+		if self._step/3 < self._led.numLEDs: 
+			# move the colours along by 3 before adding another led to the ones being lit up
+			# Fill the strip, with each sucessive color
+	        for i in range(self._step/3):
+	            self._led.set(i, self._colors[(self._step + i) % len(self._colors)])
+	    # the light strip is filled. RAINBOWS
+	    else:
+	    	for i in range(self._led.numLEDs):
+            	self._led.set(i, self._colors[(self._step + i) % len(self._colors)])
+        self._step += amt
+
+
+
+
+
+
+
+
---
+++ test.py
@@ -0,0 +1,130 @@
+#!/usr/bin/python
+import connect_wiimote
+import lights
+
+from bibliopixel.drivers.APA102 import DriverAPA102
+from bibliopixel.led import *
+
+from bibliopixel.animation import BaseStripAnim
+from bibliopixel.drivers.driver_base import ChannelOrder
+import bibliopixel.colors as colors
+
+# stuff for the wiimote
+import cwiid
+import time
+
+# -------------------
+# basic light strip
+# -------------------
+
+STRIP_METERS = 3
+LIGHTS_PER_METER = 144
+
+driver = DriverAPA102(STRIP_METERS*LIGHTS_PER_METER, c_order=ChannelOrder.GRB, SPISpeed=2)
+led = LEDStrip(driver)
+
+animation = lights.WinAnimation(led)
+
+# ---------------------------------------
+# this is all for setting up the wiimote
+# ---------------------------------------
+button_delay = 0.1
+
+print 'Press 1 + 2 on your Wii Remote now ...'
+time.sleep(1)
+
+# Connect to the Wii Remote. If it times out
+# then quit.
+try:
+  wii=cwiid.Wiimote()
+except RuntimeError:
+  print "Error opening wiimote connection"
+  quit()
+
+print 'Wii Remote connected...\n'
+print 'Press some buttons!\n'
+print 'Press PLUS and MINUS together to disconnect and quit.\n'
+
+wii.rpt_mode = cwiid.RPT_BTN
+ 
+while True:
+
+  buttons = wii.state['buttons']
+  player_position = 0
+
+  # If Plus and Minus buttons pressed
+  # together then rumble and quit.
+  if (buttons - cwiid.BTN_PLUS - cwiid.BTN_MINUS == 0):  
+    print '\nClosing connection ...'
+    wii.rumble = 1
+    time.sleep(1)
+    wii.rumble = 0
+    exit(wii)  
+  
+  # Check if other buttons are pressed by
+  # doing a bitwise AND of the buttons number
+  # and the predefined constant for that button.
+  if (buttons & cwiid.BTN_LEFT):
+    print 'Left pressed'
+    time.sleep(button_delay)         
+
+  if(buttons & cwiid.BTN_RIGHT):
+    print 'Right pressed'
+    time.sleep(button_delay)          
+
+  if (buttons & cwiid.BTN_UP):
+    print 'Up pressed'  
+    led.set(player_position, colors.Black)
+    if player_position + 1 < STRIP_METERS * LIGHTS_PER_METER:
+	    player_position += 1
+	    led.set(player_position, colors.Green)
+	else:
+		animation.run(sleep=0.1, max_steps=200)
+    time.sleep(button_delay)          
+    
+  if (buttons & cwiid.BTN_DOWN):
+    print 'Down pressed'
+    led.set(player_position, colors.Black)
+    if player_position - 1 > 0:
+	    player_position -= 1
+	    led.set(player_position, colors.Green)
+	else:
+		led.set(player_position, colors.Red)
+    time.sleep(button_delay)  
+    
+  if (buttons & cwiid.BTN_1):
+    print 'Button 1 pressed'
+    time.sleep(button_delay)          
+
+  if (buttons & cwiid.BTN_2):
+    print 'Button 2 pressed'
+    time.sleep(button_delay)          
+
+  if (buttons & cwiid.BTN_A):
+    print 'Button A pressed'
+    # sleep slows it down enough to see what's happening
+    animation.run(sleep=0.1, max_steps=200)
+    time.sleep(button_delay)          
+
+  if (buttons & cwiid.BTN_B):
+    print 'Button B pressed'
+    led.fill(colors.Black)
+    time.sleep(button_delay)          
+
+  if (buttons & cwiid.BTN_HOME):
+    print 'Home Button pressed'
+    time.sleep(button_delay)           
+    
+  if (buttons & cwiid.BTN_MINUS):
+    print 'Minus Button pressed'
+    time.sleep(button_delay)   
+    
+  if (buttons & cwiid.BTN_PLUS):
+    print 'Plus Button pressed'
+    time.sleep(button_delay)
+
+
+
+
+
+
