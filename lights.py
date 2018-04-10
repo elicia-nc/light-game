932d8ee3795a3ada0ebbbb0c7f36cd24e2b271ce
Elicia Erker <elicia@noodlecake.com>
2018-04-10 14:02:04 -0600

    fixing updates for animations

--- lights.py
+++ lights.py
@@ -16,7 +16,6 @@ class StripTest(BaseStripAnim):
 		# for every led in the strip
 		for i in range(self._led.numLEDs):
 			self._led.set(i, self._colors[(self._step + i) % len(self._colors)])
-			self._led.update()
 			# Increment the internal step by the given amount
 			self._step += amt
 
@@ -41,12 +40,11 @@ class WinAnimation(BaseStripAnim):
 			# Fill the strip, with each sucessive color
 			for i in range(self._step/3):
 				self._led.set(i, self._colors[(self._step + i) % len(self._colors)])
-				self._led.update()
+				self._step += amt
 		# the light strip is filled. RAINBOWS
 		else:
 			for i in range(self._led.numLEDs):
 				self._led.set(i, self._colors[(self._step + i) % len(self._colors)])
-				self._led.update()
 				self._step += amt
 
 
