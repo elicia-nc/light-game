887b1a893887d2214a21cca005247213cf1239a2
Elicia Erker <elicia@noodlecake.com>
2018-04-12 22:00:32 -0600

    death

--- lights.py
+++ lights.py
@@ -23,7 +23,7 @@ class StripTest(BaseStripAnim):
 class Death(BaseStripAnim):
 	def __init__(self, led, start=0, end=-1):
 		# The base class MUST be initialized by calling super like this
-		super(StripTest, self).__init__(led, start, end)
+		super(Death, self).__init__(led, start, end)
 		# Create a color array to use in the animation
 		self._colors = [colors.Red, colors.Orange, colors.Yellow]
 
